aee9a0bb0c847226ddb7864deae214fb611bfd48
Karan Meghe <karanmeghe87@gmail.com>
2022-12-01 19:14:32 +0530

    functionality adds for tabs

---
+++ script.js
@@ -0,0 +1,23 @@
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+
+// onTabClick event
+function onTabClick(e){
+ // Deactivate all tabs 
+ tabs.forEach((tab) => {
+  tab.children[0].classList.remove('border-softRed','border-b-4','md:border-b-0')
+ })
+
+//Hide all pannels
+panels.forEach((panel) => panel.classList.add('hidden'));
+
+// Avtivate new tab and pannel
+e.target.classList.add('border-softRed', 'border-b-4');
+
+const classString = e.target.getAttribute('data-target');
+document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove('hidden');
+}
+
+// tabs menu event listner
+tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+
